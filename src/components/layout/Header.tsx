6ce627177afec44405fa2efed7bb859ed017a869
[dyad] <git@dyad.sh>
2025-10-12 16:49:20 -0500

    [dyad] Created valid Header component - wrote 1 file(s)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,1 +1,11 @@
-...
+"use client";
+
+import { cn } from "@/lib/utils";
+
+export const Header = () => {
+  return (
+    <header className="bg-background border-b border-border h-16 flex items-center px-4">
+      <h1 className="text-xl font-semibold">RestaurantBot</h1>
+    </header>
+  );
+};
