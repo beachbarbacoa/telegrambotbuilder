5c1809b14306fc7bc9c41cc78a7e2e1721fb0a4a
[dyad] <git@dyad.sh>
2025-10-12 15:20:13 -0500

    Reverted all changes back to version fe82134ab082737e2c10b0cf5647bfed708b8ce1

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Signup from "./pages/auth/Signup";
-import Login from "./pages/auth/Login";
-import { DashboardLayout } from "@/components/layout/DashboardLayout";
-import Dashboard from "./pages/dashboard/Dashboard";
 
 const queryClient = new QueryClient();
 
@@ -20,15 +16,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/auth/signup" element={<Signup />} />
-          <Route path="/auth/login" element={<Login />} />
-          
-          {/* Dashboard Routes */}
-          <Route path="/dashboard" element={<DashboardLayout />}>
-            <Route index element={<Dashboard />} />
-            {/* Add more dashboard routes here */}
-          </Route>
-          
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
