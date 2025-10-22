3d19e51122603d1b33dac3195973fcfcd1fc25a7
Colin Correia <colinisnot@gmail.com>
2025-10-22 12:38:00 -0500

    Restore App and Index components with basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
 
 console.log("App: Initializing");
 
@@ -13,6 +11,8 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
